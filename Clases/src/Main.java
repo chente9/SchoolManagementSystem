8c47212eb1fed1a679ae103b63af445fda2dc813
Chente <vicente194594@gmail.com>
2024-05-01 07:24:29 -0600

    Gestor Academico, modificado clase estudiante y extendido a clase estudiante

---
+++ Clases/src/Main.java
@@ -0,0 +1,10 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.Year;
+
+public class Main {
+    public static void main (String [] argas) {
+        Estudiante estudiante1 = new Estudiante( 01, "Maria", "Carias", (2001,NOVEMBER,19), "Matriculado");
+    }
+}
