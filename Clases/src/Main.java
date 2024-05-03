c17a578a8e718ed343e9729a2f6a4f1bb7f85843
Admin <vicente194591@gmail.com>
2024-05-02 21:09:11 -0600

    algunas correcciones

--- Clases/src/Main.java
+++ Clases/src/Main.java
@@ -5,6 +5,12 @@ import java.time.Year;
 
 public class Main {
     public static void main (String [] argas) {
-        Estudiante estudiante1 = new Estudiante( 01, "Maria", "Carias", (2001,NOVEMBER,19), "Matriculado");
+        Estudiante estudiante1 = new Estudiante( 01, "Maria ", "Carias", LocalDate.of(2001, 12,19), "Matriculado");
+        Estudiante estudiante2 = new Estudiante (02, "Juan ", "Catillo ", LocalDate.of(2012, 8, 11), "No matriculado");
+        //Generar isntancias del curso
+        Curso curso1 = new Curso (010, "Estudios Sociales". "Profesor1", LocalDate.of(2024, 1, 15));
+        Curso curso2 = new Curso (011, "Ciencias Naturales", "Profesor2", LocalDate.of(2024, 2,12));
+
+
     }
 }
